ff1a958c564168741de04198c0ce3bbf2167e1ed
Steve Steyaert <stephen.a.steyaert@gmail.com>
2026-04-08 17:09:08 -0400

    registration

--- swccg-site/register/views.py
+++ swccg-site/register/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.http import HttpRequest
+from django.urls import reverse
 
 
 class CustomLoginView(LoginView):
     redirect_authenticated_user = True
 
     def get_redirect_url(self):
-        return self.request.GET.get("next") or "/home/"
+        return self.request.GET.get("next") or reverse('home')
+
+
+def register(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 
 class ProtectedLogoutView(LoginRequiredMixin, LogoutView):
--- swccg-site/swccgdb/views.py
+++ swccg-site/swccgdb/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    return render(request, 'swccgdb/index.html')
+
+
+@login_required
+def home(request):
+    return render(request, 'swccgdb/home.html')
+
