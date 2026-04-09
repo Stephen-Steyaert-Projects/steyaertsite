77922b45b51772303e0cc0da88e02412e2bd8894
Steve Steyaert <stephen.a.steyaert@gmail.com>
2026-04-08 21:16:34 -0400

    whole lot of card updates

--- swccg-site/swccgdb/models.py
+++ swccg-site/swccgdb/models.py
@@ -16,19 +16,40 @@ class Card(models.Model):
         DARK = 'D', 'Dark Side'
 
     class CardType(models.TextChoices):
+        ADMIRALS_ORDER = 'admirals_order', "Admiral's Order"
         CHARACTER = 'character', 'Character'
+        CREATURE = 'creature', 'Creature'
         DEVICE = 'device', 'Device'
         EFFECT = 'effect', 'Effect'
+        EPIC_EVENT = 'epic_event', 'Epic Event'
         INTERRUPT = 'interrupt', 'Interrupt'
+        JEDI_TEST = 'jedi_test', 'Jedi Test'
         LOCATION = 'location', 'Location'
+        OBJECTIVE = 'objective', 'Objective'
         STARSHIP = 'starship', 'Starship'
         VEHICLE = 'vehicle', 'Vehicle'
         WEAPON = 'weapon', 'Weapon'
+
+    class Rarity(models.TextChoices):
+        C = 'C', 'C'
+        C1 = 'C1', 'C1'
+        C2 = 'C2', 'C2'
+        U = 'U', 'U'
+        U1 = 'U1', 'U1'
+        U2 = 'U2', 'U2'
+        R = 'R', 'R'
+        R1 = 'R1', 'R1'
+        R2 = 'R2', 'R2'
+        UR = 'UR', 'UR'
+        XR = 'XR', 'XR'
+        F = 'F', 'F'
+        PM = 'PM', 'PM'
 
     name = models.CharField(max_length=200)
     card_set = models.ForeignKey(Set, on_delete=models.PROTECT, related_name='cards')
     card_type = models.CharField(max_length=20, choices=CardType.choices)
     side = models.CharField(max_length=1, choices=Side.choices)
+    rarity = models.CharField(max_length=2, choices=Rarity.choices, blank=True)
     image = models.ImageField(upload_to='cards/', blank=True, null=True)
 
     def __str__(self):
