52c628d3c91f0016528e1528121b29b130d9f903
Steve Steyaert <stephen.a.steyaert@gmail.com>
2026-04-15 23:04:57 -0400

    admin

--- swccg-site/swccgdb/admin.py
+++ swccg-site/swccgdb/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Card, Set
 
-# Register your models here.
+
+@admin.register(Set)
+class SetAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_filter = ['card_set', 'card_type', 'side']
