e8ea3aea7d692809206830869d766530ff78c513
Steve Steyaert <stephen.a.steyaert@gmail.com>
2026-04-15 22:20:17 -0400

    fix typo

--- swccg-site/swccg/settings/__init__.py
+++ swccg-site/swccg/settings/__init__.py
@@ -4,7 +4,7 @@ def get_secret(secret_id, backup=None):
     return os.getenv(secret_id, backup)
 
 if get_secret('PIPELINE') == 'production':
-    from .production import *
+    from .prod import *
 
 else:
     from .dev import *
