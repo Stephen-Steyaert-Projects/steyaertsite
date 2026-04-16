8c1a2dcaf42ee81a3f76b2ef38502cf5092b6fa3
Steve Steyaert <stephen.a.steyaert@gmail.com>
2026-04-15 23:15:19 -0400

    updates

--- swccg-site/register/urls.py
+++ swccg-site/register/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomLoginView, ProtectedLogoutView, register
+from django.contrib.auth.views import LogoutView
+from .views import CustomLoginView, register
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', ProtectedLogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
 ]
--- swccg-site/register/views.py
+++ swccg-site/register/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.http import HttpRequest
@@ -26,10 +25,3 @@ def register(request: HttpRequest):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
-
-
-class ProtectedLogoutView(LoginRequiredMixin, LogoutView):
-    login_url = "/auth/login/"  # or whatever your login URL is
-
-    def handle_no_permission(self):
-        return redirect('index')
